de18fa4039eecdb936fb715904214603d2095c35
signalwerk <sh@signalwerk.ch>
2017-11-04 16:56:18 +0100

    ADD: grid support

---
+++ doc/Gulpfile.js
@@ -0,0 +1,62 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const cssnano = require('gulp-cssnano');
+const ignore = require('gulp-ignore');
+
+
+var base = './';
+
+var input = base + 'assets/main.scss';
+var observe = base + '../**/*.scss';
+var output = base + '/public';
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+
+gulp.task('sass', function () {
+  return gulp
+    .src(input)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    // .pipe(sourcemaps.write())  >> but in current release see next
+    .pipe(sourcemaps.write(undefined, { sourceRoot: null }))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(output))
+
+    // now produce the min version
+    .pipe(rename({
+        suffix: '.min'
+    }))
+
+    .pipe(cssnano())
+
+    .pipe(gulp.dest(output))
+
+    // Release the pressure back and trigger flowing mode (drain)
+    // See: http://sassdoc.com/gulp/#drain-event
+    .resume();
+});
+
+
+
+gulp.task('watch', function() {
+  return gulp
+    // Watch the input folder for change,
+    // and run `sass` task when something happens
+    .watch(observe, ['sass'])
+    // When there is a change,
+    // log a message in the console
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+
+
+gulp.task('default', ['sass', 'watch' /*, possible other tasks... */]);
